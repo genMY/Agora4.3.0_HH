17957dd662cd5ad79c591786446e0cb692bb5327
genMY <yangmou85@outlook.com>
2023-11-13 23:50:05 +0800

    add hh code
    业务代码

---
+++ huohua_4.3.0/iOS/HHAgoraAdapter.h
@@ -0,0 +1,19 @@
+//
+//  HHAgoraAdapter.h
+//  HHLiveSDK
+//
+//  Created by lijunjie on 2020/9/9.
+//  Copyright © 2020 lijunjie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <HHLiveSDK/HHBaseAdapter.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Agora SDK 适配器
+@interface HHAgoraAdapter : HHBaseAdapter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ huohua_4.3.0/iOS/HHAgoraVideoRender.h
@@ -0,0 +1,21 @@
+//
+//  HHAgoraVideoRender.h
+//  HHLiveSDK
+//
+//  Created by changjingjing on 2020/10/20.
+//  Copyright © 2020 changjingjing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AgoraRtcKit/AgoraRtcEngineKit.h>
+#import <HHLiveSDK/HHLiveVideoSinkProtocol.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHAgoraVideoRender : NSObject <AgoraVideoSinkProtocol>
+
+@property (nonatomic, strong) id <HHLiveVideoSinkProtocol> videoRenderer;
+
+@end
+
+NS_ASSUME_NONNULL_END
